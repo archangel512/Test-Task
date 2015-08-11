2b8a2b6e675d10278f254a2c2b4ae48680ed4ae7
archangel512 <koshak512@gmail.com>
2015-08-11 14:04:35 +0400

    First commit

---
+++ Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double triangleSide_1, triangleSide_2, triangleSide_3;
+
+            try
+            {
+                Console.WriteLine("Side_1 = ");
+                triangleSide_1 = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("Side_2 = ");
+                triangleSide_2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Side_3 = ");
+                triangleSide_3 = Convert.ToDouble(Console.ReadLine());
+
+                bool treangleExist = (triangleSide_1 < triangleSide_2 + triangleSide_3) && (triangleSide_2 < triangleSide_1 + triangleSide_3) && (triangleSide_3 < triangleSide_1 + triangleSide_2);
+
+                if (treangleExist)
+                {
+
+                    Triangle newTriangle = new Triangle(triangleSide_1, triangleSide_2, triangleSide_3);
+                    
+                    Console.WriteLine("Area = " + newTriangle.Area());
+                }
+                else
+                    throw new Exception("Triangle does not exist");
+            }
+           
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Test/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test
+{
+    public class Triangle
+    {
+        private double side_1 { get; set; }
+        private double side_2 { get; set; }
+        private double side_3 { get; set; }
+
+        public Triangle() { }
+        public Triangle(double side_1, double side_2, double side_3)
+        {
+            this.side_1 = side_1;
+            this.side_2 = side_2;
+            this.side_3 = side_3;
+        }
+
+        public double Perimeter()
+        {
+            return side_1 + side_2 + side_3;
+        }
+
+        public double Area()
+        {
+            double area, semiperimeter = this.Perimeter() / 2;            
+            area = System.Math.Sqrt(semiperimeter * (semiperimeter - side_1) * (semiperimeter - side_2) * (semiperimeter - side_3));
+            return area;
+        }
+
+    }
+}
